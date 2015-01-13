c882e05ddc21c7ec0987b0b69fb366916dabf137
shubhsin <luk4shubhs@gmail.com>
2015-01-13 10:32:21 +0530

    Initial Commit

---
+++ ScrollViews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViews
+//
+//  Created by Shubham Sorte on 13/01/15.
+//  Copyright (c) 2015 LUGManipal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViews
+//
+//  Created by Shubham Sorte on 13/01/15.
+//  Copyright (c) 2015 LUGManipal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
